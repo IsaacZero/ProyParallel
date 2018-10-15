0f7166cc460e74f58117fd03f5002724078314b4
Isaac Tretta <32001785+IsaacZero@users.noreply.github.com>
2018-10-14 21:14:09 -0600

    Ya solo faltan las pruebas :v

--- SimVirus/SimVirus/main.cpp
+++ SimVirus/SimVirus/main.cpp
@@ -77,15 +77,26 @@ int main(int argc, char* argv[]) {
 	Simulador matrix(personas, infectados, cntMuerte, potInfecc, potRecup, size);
 	archivoSalida << "Cantidades Originales." << endl;
 	archivoSalida << "Personas Suceptibles: " << (personas - infectados) << endl;
-	archivoSalida << "Infectados: " << infectados << endl;
+	archivoSalida << "Infectados: " << (personas * infectados)/100 << endl;
 	archivoSalida << "Cantidad de Tics: " << totalTics << endl;
-	archivoSalida << "Tamaño de la civilizacion: " << size << "X" << size << endl;
+	archivoSalida << "Tamaño de la civilizacion: " << size << " X " << size << endl;
+	archivoSalida << omp_get_max_threads() << endl;
 	infectados = muertos = susceptiblesTic = recuperados = susceptibles = 0;
 	//inicia tiempo
-	steady_clock::time_point t1 = steady_clock::now();
+	/*steady_clock::time_point iniciar;
+	iniciar = steady_clock::now();*/
+	steady_clock::time_point t1;
+	steady_clock::time_point t2;
+	duration<double> time_tic;
+	duration<double> time_span;
 	for (int i = 1; i <= totalTics; i++) {
+		t1 = steady_clock::now();
 		matrix.generarTic();
 		matrix.calcularInfeciones();
+		t2 = steady_clock::now();
+		time_span += duration_cast<duration<double>>(t2 - t1);
+		if (matrix.getCantInfect() == 0)
+			time_tic = time_span;
 		infectados += matrix.getCantInfect();
 		muertos += matrix.getCantMuertos();
 		recuperados += matrix.getCantRecup();
@@ -111,27 +122,39 @@ int main(int argc, char* argv[]) {
 		archivoSalida << endl;
 	}
 	//termina tiempo
-	steady_clock::time_point t2 = steady_clock::now();
+	//steady_clock::time_point t2 = steady_clock::now();
 	//imprime duracion
-	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-	std::cout << "Duracion " << time_span.count() << " segundos.";
+	//duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+	archivoSalida << "Duracion para la estabilizacion: " << time_span.count() << " segundos." << endl;
+	cout << "Duracion para la estabilizacion: " << time_span.count() << " segundos." << endl;
+	archivoSalida << "Duracion total: " << time_span.count() << " segundos." << endl;
+	std::cout << "Duracion total: " << time_span.count() << " segundos.";
 	cin >> personas;
 	return 0;
 }
 
 bool validaCntTics(int ct)
 {
-	return ct >= 1;
+	bool valido = true;
+	if(ct < 1)
+		cout << "Por Favor Digite una cantidad Valida." << endl;
+	return valido;
 }
 
 bool validaCntPersonas(int ct)
 {
-	return ct >= 1;
+	bool valido = true;
+	if(ct < 1)
+		cout << "Por Favor Digite una cantidad Valida." << endl;
+	return valido;
 }
 
 bool validaCntInfectados(int ct)
 {
-	return ct >= 1;
+	bool valido = true;
+	if(ct < 1)
+		cout << "Por Favor Digite una cantidad Valida." << endl;
+	return valido;
 }
 
 bool validaSize(int ct)
@@ -139,6 +162,8 @@ bool validaSize(int ct)
 	bool succes = false;
 	if (ct >= 1 && ct <= 3)
 		succes = true;
+	else
+		cout << "Por Favor Digite una opcion valida." << endl;
 	return succes;
 }
 
@@ -147,6 +172,8 @@ bool validaProInfec(double ct)
 	bool succes = false;
 	if (ct > 0.0 && ct < 1.0)
 		succes = true;
+	else
+		cout << "Por Favor Digite una probabilidad valida." << endl;
 	return succes;
 }
 
@@ -155,10 +182,15 @@ bool validaProbRec(double ct)
 	bool succes = false;
 	if (ct > 0.0 && ct < 1.0)
 		succes = true;
+	else
+		cout << "Por Favor Digite una probabilidad valida." << endl;
 	return succes;
 }
 
 bool validaCntMuerte(int ct)
 {
-	return ct >= 1;
+	bool valido = true;
+	if(ct < 1)
+		cout << "Por Favor Digite una cantidad Valida." << endl;
+	return valido;
 }
